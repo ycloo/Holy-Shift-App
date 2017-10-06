cccf3135c1e95c5d895b78987f987f759e4f8f6c
ycloo <yc.lu95@gmail.com>
2017-10-06 14:56:40 -0700

    added basic team and shift objects

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  belongs_to :team
+  has_and_belongs_to_many :shifts
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
