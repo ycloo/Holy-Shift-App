1ef4f4dd676749c5e34f09e7afa209e9ca8495b5
JaredTan <jared.e.tan@gmail.com>
2017-11-17 14:21:23 -0800

    api route

--- HShiftApp/frontend/api/index.js
+++ HShiftApp/frontend/api/index.js
@@ -1,6 +1,8 @@
 const API_URL = 'http://localhost:3000/';
 export const SIGNIN_URL = `${API_URL}/login`;
 export const SIGNUP_URL = `${API_URL}/signup`;
-export const USERS_URL = (user_id) => `${API_URL}/users/${user_id}`;
-export const TEAM_URL = (team_id) => `${API_URL}/teams/${team_id}`;
+export const USERS_URL = (userId) => `${API_URL}/users/${userId}`;
+export const TEAM_URL = (teamId) => `${API_URL}/teams/${teamId}`;
 export const TEAMS_URL = `${API_URL}/teams/`;
+export const SHIFTS_URL = `${API_URL}/shifts/`;
+export const SHIFT_URL = shiftId => `${API_URL}/shifts/${shiftId}`
